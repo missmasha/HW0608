bfd5f14d515a427a651fa6bc2630557742498c34
Maria Andreeva <maria.serg.andreeva@gmail.com>
2020-08-09 12:42:42 +0300

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "FoodCalculator"
+
+version := "0.1"
+
+scalaVersion := "2.13.3"
---
+++ src/main/scala/FoodCalculator.scala
@@ -0,0 +1,10 @@
+object FoodCalculator extends App {
+  val food = "potatoes"
+  val price = 0.37
+  val quantity = 20
+
+  print("It will cost " + price * quantity)
+  print(" eur to buy " + quantity)
+  println(" kilos of " + food)
+
+}
