71e295a530d609e7c64783ba782189f91a3710bd
Maria Andreeva <maria.serg.andreeva@gmail.com>
2020-08-10 19:14:28 +0300

    Made Food Calculator with 3 command arguments

--- src/main/scala/FoodCalculator.scala
+++ src/main/scala/FoodCalculator.scala
@@ -6,4 +6,15 @@ object FoodCalculator extends App {
   print("It will cost " + price * quantity)
   println(s" eur to buy $quantity kilos of $food")
 
+
+  println(s"We got ${args.length} arguments")
+  println(s"The first argument is ${args(0)}")
+  println(s"The second argument is ${args(1)}")
+  println(s"The third argument is ${args(2)}")
+  val cost = args(0).toFloat * args(1).toInt
+  println(s"It will cost $cost eur to buy ${args(1)} kilos of ${args(2)}")
+
+
+  //TODO make this Calculator more generatic
+  //FIXME fix this mistake
 }
